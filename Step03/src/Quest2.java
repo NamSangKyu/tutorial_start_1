506044edc4154b53e5c148c3e0ddf510060cae5a
NamSangKyu <nam2626@gmail.com>
2021-05-24 17:47:56 +0900

    반복문 예제

--- Step03/src/Quest2.java
+++ Step03/src/Quest2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Quest2 {
 
@@ -7,7 +8,24 @@ public class Quest2 {
 		 * 두수의 뺼셈 결과를 출력
 		 * 단, 무조건 결과값이 양수가 나오겠끔 계산하시오.
 		 */
+		int n1, n2;
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("첫번째 숫자 입력 : ");
+		n1 = sc.nextInt();
+		System.out.print("두번째 숫자 입력 : ");
+		n2 = sc.nextInt();
+		//답안1
+//		int result = n1 > n2 ? n1 - n2 : n2 - n1;
+		
+		//답안2
+		int result = n1 - n2;
+		result = result < 0 ? -result : result;		
+		
+		System.out.println(result);
 	}
 
 }
+
+
+
